62a005c5b35f06ebaf72ed3582b4e79270cf29a1
mvoss9000 <mvoss9000@yahoo.com>
2020-01-14 07:31:16 -0500

    add performance test

--- export/perf.js
+++ export/perf.js
@@ -24,15 +24,20 @@ function parse (buf) {
   var t0 = new Date()
   var ps = {src: buf}
   while (next(ps)) {}
-  var t1 = new Date()
-  console.log('parsed ' + (buf.length/(1024*1024)) + ' MB in', (t1 - t0)/1000, 'seconds')
+  return new Date() - t0;
 }
 
 var fname = '/Users/dad/dev/json-samples/cache_150mb.json'
 var buf = fs.readFileSync(fname)
 console.log('read', fname)
 
-for (var i=0; i<5; i++) {
-  parse(buf)
+var total_ms = 0
+var iter = 5
+for (var i=0; i<iter; i++) {
+  var ms = parse(buf)
+  console.log('parsed ' + (buf.length/(1024*1024)) + ' MB in', ms/1000, 'seconds')
+  total_ms += ms
 }
+
+console.log(iter * buf.length / ((total_ms/1000) * 1024 * 1024) + ' MB/second')
 
